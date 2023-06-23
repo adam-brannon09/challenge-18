677eeea866c6c0919f630c44245dc4956ad8f8db
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-06-23 16:41:06 -0500

    initial commit

---
+++ models/Course.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const courseSchema = new Schema(
+  {
+    courseName: {
+      type: String,
+      required: true,
+    },
+    inPerson: {
+      type: Boolean,
+      default: true,
+    },
+    startDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    endDate: {
+      type: Date,
+      // Sets a default value of 12 weeks from now
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+// courseSchema.virtual('studentCount').get(function () {
+//   return this.students.length
+// })
+
+
+
+const Course = model('Course', courseSchema);
+
+module.exports = Course;
